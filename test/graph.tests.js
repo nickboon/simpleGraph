5877437f52ac4d3df6bc74b31ed10abeec0391e0
nickboon <boonnick@hotmail.com>
2019-02-18 20:37:52 +0000

    Update graph.tests.js

--- test/graph.tests.js
+++ test/graph.tests.js
@@ -23,24 +23,9 @@ test('new Graph().svg', assert => {
 });
 
 test('new Graph().setColour(colour).setFigures(figures).svg', assert => {
-    assert.equal(
-        new Sut().setColour('black').setFigures([
-            new Point().label()
-        ]).svg,
-        [
-            '<?xml version="1.0" encoding="UTF-8" standalone="no"?>',
-            '<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">',
-            '<svg ', [
-                'version="1.1" ',
-                'xmlns="http://www.w3.org/2000/svg" ',
-                'xmlns:xlink="http://www.w3.org/1999/xlink" ',
-                'width="undefined" height="undefined"'
-            ].join(''),
-            '>', [
-                '<text x="0" y="0" fill="undefined" opacity="undefined>0 0</text>'
-            ].join(''),
-            '</svg>'
-        ].join(''),
+    assert.true(
+        new Sut().setColour('black').setFigures([new Point().label()]).svg
+        .includes('<text x="0" y="0" fill="undefined" opacity="undefined>0 0</text>'),
         'should add figures to the returned SVG string.'
     );
     assert.end();
